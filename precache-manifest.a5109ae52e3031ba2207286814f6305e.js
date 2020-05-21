96b5d7aa4961c381474450c9b620c5ef00fdebef
yuanhao <yuanhao@smallrig.com>
2020-05-21 13:40:39 +0800

    测试

---
+++ precache-manifest.a5109ae52e3031ba2207286814f6305e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e96d94eca27db4ecdb4",
+    "url": "/static/css/main.0bda41f9.chunk.css"
+  },
+  {
+    "revision": "4e96d94eca27db4ecdb4",
+    "url": "/static/js/main.2f3ea318.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f35a641e04979c057c7e",
+    "url": "/static/css/2.7d78e181.chunk.css"
+  },
+  {
+    "revision": "f35a641e04979c057c7e",
+    "url": "/static/js/2.76b72e6c.chunk.js"
+  },
+  {
+    "revision": "0e8a3f5d9e9ca124c1d648455b578bf5",
+    "url": "/index.html"
+  }
+];
